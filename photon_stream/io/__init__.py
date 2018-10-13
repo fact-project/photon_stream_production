5828b500abcddc7e3b46cce18c9470cc1d4f5510
relleums <sebmuell@phys.ethz.ch>
2018-10-13 11:08:02 +0200

    rm photon_stream, only keep production

--- photon_stream/io/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import jsonl
-from . import binary
-from . import magic_constants
-from .is_gzipped_file import is_gzipped_file
--- photon_stream/simulation_truth/__init__.py
+++
@@ -1,3 +0,0 @@
-from .simulation_truth import SimulationTruth
-from . import corsika_headers
-from .air_shower_truth import AirShowerTruth
