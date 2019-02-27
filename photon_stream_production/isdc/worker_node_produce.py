1f4e35f122a5e447b8f3a2cb47ee3c8eea5912df
sebastian <sebmuell@phys.ethz.ch>
2019-02-27 16:56:10 +0100

    renaming the path does not help too much

--- photon_stream_production/isdc/worker_node_produce.py
+++ photon_stream_production/isdc/worker_node_produce.py
@@ -2,48 +2,48 @@
 Usage: phs.obs.production.isdc.worker [options]
 
 Options:
-        --java_path=PATH            [default: /home/guest/relleums2/java8/jdk1.8.0_111]
-        --fact_tools_jar_path=PATH  [default: /home/guest/relleums2/fact_photon_stream/fact-tools/target/fact-tools-0.18.1.jar]
-        --fact_tools_xml_path=PATH  [default: /home/guest/relleums2/fact_photon_stream/photon_stream/photon_stream/production/resources/observations_pass4.xml]
-        --raw_path=PATH             [default: /fact/raw/2017/09/01/20170901_139.fits.fz]
-        --drs_path=PATH             [default: /fact/raw/2017/09/01/20170901_129.drs.fits.gz]
-        --aux_dir=PATH              [default: /fact/aux/2017/09/01/]
-        --out_dir=PATH              [default: /home/guest/relleums2/qsub]
-        --out_basename=PATH         [default: 20170901_139]
-        --tmp_dir_basename=NAME     [default: phs_obs_]
-    """
-    import docopt
-    import os
-    from glob import glob
-    from os.path import join
-    import subprocess
-    import tempfile
-    import shutil
+    --java_path=PATH            [default: /home/guest/relleums/java8/jdk1.8.0_111]
+    --fact_tools_jar_path=PATH  [default: /home/guest/relleums/fact_photon_stream/fact-tools/target/fact-tools-0.18.1.jar]
+    --fact_tools_xml_path=PATH  [default: /home/guest/relleums/fact_photon_stream/photon_stream/photon_stream/production/resources/observations_pass4.xml]
+    --raw_path=PATH             [default: /fact/raw/2017/09/01/20170901_139.fits.fz]
+    --drs_path=PATH             [default: /fact/raw/2017/09/01/20170901_129.drs.fits.gz]
+    --aux_dir=PATH              [default: /fact/aux/2017/09/01/]
+    --out_dir=PATH              [default: /home/guest/relleums/qsub]
+    --out_basename=PATH         [default: 20170901_139]
+    --tmp_dir_basename=NAME     [default: phs_obs_]
+"""
+import docopt
+import os
+from glob import glob
+from os.path import join
+import subprocess
+import tempfile
+import shutil
 
 
-    def run(
-        java_path,
-        fact_tools_jar_path,
-        fact_tools_xml_path,
-        raw_path,
-        drs_path,
-        aux_dir,
-        out_dir,
-        out_basename,
-        tmp_dir_basename,
-    ):
-        with tempfile.TemporaryDirectory(prefix=tmp_dir_basename) as tmp:
+def run(
+    java_path,
+    fact_tools_jar_path,
+    fact_tools_xml_path,
+    raw_path,
+    drs_path,
+    aux_dir,
+    out_dir,
+    out_basename,
+    tmp_dir_basename,
+):
+    with tempfile.TemporaryDirectory(prefix=tmp_dir_basename) as tmp:
 
-            my_env = os.environ.copy()
-            my_env["PATH"] = java_path + my_env["PATH"]
+        my_env = os.environ.copy()
+        my_env["PATH"] = java_path + my_env["PATH"]
 
-            subprocess.call([
-                'java',
-                '-XX:MaxHeapSize=1024m',
-                '-XX:InitialHeapSize=512m',
-                '-XX:CompressedClassSpaceSize=64m',
-                '-XX:MaxMetaspaceSize=128m',
-                '-XX:+UseConcMarkSweepGC',
+        subprocess.call([
+            'java',
+            '-XX:MaxHeapSize=1024m',
+            '-XX:InitialHeapSize=512m',
+            '-XX:CompressedClassSpaceSize=64m',
+            '-XX:MaxMetaspaceSize=128m',
+            '-XX:+UseConcMarkSweepGC',
             '-XX:+UseParNewGC',
             '-jar', fact_tools_jar_path, fact_tools_xml_path,
             '-Dinfile=file:'+raw_path,
